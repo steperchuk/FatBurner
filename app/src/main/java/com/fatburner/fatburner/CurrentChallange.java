f56b64ca6ebbd1f0ea342e85ded86e5113ee376c
Sergey Teperchuk <sergey.teperchuk@mcdean.com>
2017-05-24 00:53:10 +0300

    Updated menu. Added backward navigation to the Main Activity. Fixed typos.

--- app/src/main/java/com/fatburner/fatburner/CurrentChallange.java
+++ app/src/main/java/com/fatburner/fatburner/CurrentChallange.java
@@ -1,18 +1,14 @@
 package com.fatburner.fatburner;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MenuItem;
 
-public class CurrentChalange extends AppCompatActivity {
+public class CurrentChallange extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_current_chalange);
+        setContentView(R.layout.activity_current_challange);
     }
-
-
 
 }
--- app/src/main/java/com/fatburner/fatburner/MainActivity.java
+++ app/src/main/java/com/fatburner/fatburner/MainActivity.java
@@ -1,6 +1,7 @@
 package com.fatburner.fatburner;
 
 import android.content.Intent;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToogle;
+    private NavigationView mNavigationView;
 
 
     @Override
@@ -19,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        mNavigationView = (NavigationView) findViewById(R.id.navigation_view);
         mToogle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
 
         mDrawerLayout.addDrawerListener(mToogle);
@@ -26,48 +29,60 @@ public class MainActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        setupDrawerContent(mNavigationView);
+
 
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(mToogle.onOptionsItemSelected(item)){
+        if (mToogle.onOptionsItemSelected(item)) {
             return true;
         }
-
-        Intent intent;
-        switch(item.getItemId()) {
-            case R.id.nav_achivements:
-                intent = new Intent(MainActivity.this, MyAchivement.class);
-                startActivity(intent);
-                break;
-            case R.id.nav_chanlange:
-                intent = new Intent(MainActivity.this, CurrentChalange.class);
-                startActivity(intent);
-                break;
-            case R.id.nav_calendar:
-                intent = new Intent(MainActivity.this, MealCalendar.class);
-                startActivity(intent);
-                break;
-            case R.id.nav_diet:
-                intent = new Intent(MainActivity.this, Diet.class);
-                startActivity(intent);
-                break;
-            case R.id.nav_programs:
-                intent = new Intent(MainActivity.this, CurrentChalange.class);
-                startActivity(intent);
-                break;
-            case R.id.nav_settings:
-                intent = new Intent(MainActivity.this, Settings.class);
-                startActivity(intent);
-                break;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-
         return super.onOptionsItemSelected(item);
     }
 
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        selectDrawerItem(menuItem);
+                        return true;
+                    }
+                });
+    }
 
-}
+    public void selectDrawerItem(MenuItem item){
+            Intent intent;
+            switch (item.getItemId()) {
+                case R.id.nav_achivements:
+                    intent = new Intent(MainActivity.this, MyAchivement.class);
+                    startActivity(intent);
+                    break;
+                case R.id.nav_challange:
+                    intent = new Intent(MainActivity.this, CurrentChallange.class);
+                    startActivity(intent);
+                    break;
+                case R.id.nav_calendar:
+                    intent = new Intent(MainActivity.this, MealCalendar.class);
+                    startActivity(intent);
+                    break;
+                case R.id.nav_diet:
+                    intent = new Intent(MainActivity.this, Diet.class);
+                    startActivity(intent);
+                    break;
+                case R.id.nav_programs:
+                    intent = new Intent(MainActivity.this, MyPrograms.class);
+                    startActivity(intent);
+                    break;
+                case R.id.nav_settings:
+                    intent = new Intent(MainActivity.this, Settings.class);
+                    startActivity(intent);
+                    break;
+            }
+        }
+
+    }
+
 
