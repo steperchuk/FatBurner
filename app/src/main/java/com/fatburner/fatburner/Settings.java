c96fc59c6556b78ca75166768a81a5069ea44856
Sergey Teperchuk <sergey.teperchuk@mcdean.com>
2017-06-06 23:22:10 +0300

    Updated Settings activity.

--- app/src/main/java/com/fatburner/fatburner/Settings.java
+++ app/src/main/java/com/fatburner/fatburner/Settings.java
@@ -1,25 +1,48 @@
 package com.fatburner.fatburner;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Color;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.AppCompatActivity;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.text.InputType;
+import android.util.Log;
+import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
-import android.widget.RadioButton;
+import android.widget.ListView;
 import android.widget.Switch;
+import android.widget.TimePicker;
 
-import java.security.Provider;
+import java.util.Calendar;
+
 
 public class Settings extends Menu {
 
-    SettingsProvider settings = new SettingsProvider();
+    Calendar calander = Calendar.getInstance();
+
+    String days[] = { "Monday", "Thuesday", "Wensday", "Thursday", "Friday", "Saturday", "Sunday" };
+    boolean chkd[] = { false, false, false, false, false, false, false };
+    String daysSelected = "";
+
+    int DAY_START_TIME = 1;
+    int TRAIN_START_TIME = 2;
+    final int DIALOG_DAYS = 3;
+    int myHour =  calander.get(Calendar.HOUR);
+    int myMinute = calander.get(Calendar.MINUTE);
+    EditText dayStartTime;
+    EditText trainingStart;
+    EditText trainingDays;
+
+
+    SettingsProvider settings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,25 +55,58 @@ public class Settings extends Menu {
         mDrawerLayout.addView(contentView, 0);
 
 
+
+
         final Button applyButton = (Button) findViewById(R.id.applyButton);
         final Switch gender = (Switch) findViewById(R.id.gender);
         final EditText age = (EditText) findViewById(R.id.age);
         final EditText weight = (EditText) findViewById(R.id.weight);
-        final EditText dayStartTime = (EditText) findViewById(R.id.dayStart);
-        final EditText trainingStart = (EditText) findViewById(R.id.trainingStart);
-        final CheckBox monday = (CheckBox) findViewById(R.id.monday);
-        final CheckBox thuesday = (CheckBox) findViewById(R.id.thuesday);
-        final CheckBox wensday = (CheckBox) findViewById(R.id.wensday);
-        final CheckBox thursday = (CheckBox) findViewById(R.id.thursday);
-        final CheckBox friday = (CheckBox) findViewById(R.id.friday);
-        final CheckBox saturday = (CheckBox) findViewById(R.id.saturday);
-        final CheckBox sunday = (CheckBox) findViewById(R.id.sunday);
+        dayStartTime = (EditText) findViewById(R.id.dayStart);
+        trainingStart = (EditText) findViewById(R.id.trainingStart);
+        trainingDays = (EditText) findViewById(R.id.trainingDays);
         final Switch switchWaterNotification = (Switch) findViewById(R.id.switchWaterNotification);
         final Switch switchFoodNotification = (Switch) findViewById(R.id.switchFoodNotification);
         final Switch switchSleepNotification = (Switch) findViewById(R.id.switchSleepNotification);
 
-        SettingsProvider initialSettings = db.getSettings();
+        dayStartTime.setInputType(InputType.TYPE_NULL);
+        trainingStart.setInputType(InputType.TYPE_NULL);
+        trainingDays.setInputType(InputType.TYPE_NULL);
 
+
+        dayStartTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hideKeyboard(v);
+                showDialog(DAY_START_TIME);
+            }
+        });
+
+
+        trainingStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hideKeyboard(v);
+                showDialog(TRAIN_START_TIME);
+            }
+        });
+
+        trainingDays.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hideKeyboard(v);
+                trainingDays.setText("");
+                daysSelected = "";
+                showDialog(DIALOG_DAYS);
+            }
+        });
+
+
+
+        SettingsProvider initialSettings = db.getSettings();
+        settings = initialSettings;
+
+        age.setText(initialSettings.getAge());
+        weight.setText(initialSettings.getWeight());
         /*
         ////////////////////////// values from db ////////////////
 
@@ -69,6 +125,9 @@ public class Settings extends Menu {
 
         gender.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(gender.getText().equals("Male"))
+                {gender.setText("Female");}
+                else {gender.setText("Male");}
                 settings.setGender(isChecked);
             }
         });
@@ -91,125 +150,9 @@ public class Settings extends Menu {
             }
         });
 
-        final String[] trainingDays = new String [7];
-
-        monday.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
-        {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
-            {
-                if ( isChecked )
-                {
-                    trainingDays[0] = "Mon";
-                }
-                else
-                {
-                    trainingDays[0] = "";
-                }
-            }
-        });
-
-        thuesday.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
-        {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
-            {
-                if ( isChecked )
-                {
-                    trainingDays[1] = "Tue";
-                }
-                else
-                {
-                    trainingDays[1] = "";
-                }
-
-            }
-        });
-
-        wensday.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
-        {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
-            {
-                if ( isChecked )
-                {
-                    trainingDays[2] = "Wen";
-                }
-                else
-                {
-                    trainingDays[2] = "";
-                }
-
-            }
-        });
-
-        thursday.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
-        {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
-            {
-                if ( isChecked )
-                {
-                    trainingDays[3] = "Thu";
-                }
-                else
-                {
-                    trainingDays[3] = "";
-                }
-
-            }
-        });
-
-        friday.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
-        {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
-            {
-                if ( isChecked )
-                {
-                    trainingDays[4] = "Fri";
-                }
-                else
-                {
-                    trainingDays[4] = "";
-                }
-
-            }
-        });
-
-        saturday.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
-        {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
-            {
-                if ( isChecked )
-                {
-                    trainingDays[5] = "Sat";
-                }
-                else
-                {
-                    trainingDays[5] = "";
-                }
-
-            }
-        });
-
-        sunday.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
-        {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
-            {
-                if ( isChecked )
-                {
-                    trainingDays[6] = "Sun";
-                }
-                else
-                {
-                    trainingDays[6] = "";
-                }
-
-            }
-        });
 
 
-        settings.setTrainingDays(trainingDays.toString());
-        settings.setDayStartTime(dayStartTime.getText().toString());
-        settings.setTrainingStartTime(trainingStart.getText().toString());
-        settings.setAge(age.getText().toString());
-        settings.setWeight(weight.getText().toString());
+
 
         applyButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -219,15 +162,98 @@ public class Settings extends Menu {
                     applyButton.setText("Apply");
                 }
 
+                settings.setAge(age.getText().toString());
+                settings.setWeight(weight.getText().toString());
+                settings.setTrainingDays(trainingDays.toString());
+                settings.setDayStartTime(dayStartTime.getText().toString());
+                settings.setTrainingStartTime(trainingStart.getText().toString());
+
+
                 db.addSettings(settings);
-                //Intent intent = new Intent(Settings.this, MainActivity.class);
-                //startActivity(intent);
+                Intent intent = new Intent(Settings.this, MainActivity.class);
+                startActivity(intent);
 
             }
         });
 
 
 
-
     }
+
+    protected Dialog onCreateDialog(int id) {
+        AlertDialog.Builder adb = new AlertDialog.Builder(this);
+
+        if (id == DAY_START_TIME) {
+            TimePickerDialog tpd = new TimePickerDialog(this, myCallBack, myHour, myMinute, false);
+            return tpd;
+        }
+        if (id == TRAIN_START_TIME) {
+            TimePickerDialog tpd = new TimePickerDialog(this, mySecondCallBack, myHour, myMinute, false);
+            return tpd;
+        }
+        if (id == DIALOG_DAYS)
+        {
+            adb.setTitle(R.string.items);
+            adb.setMultiChoiceItems(days, chkd, myItemsMultiClickListener);
+            adb.setPositiveButton(R.string.ok, myBtnClickListener);
+            return adb.create();
+        }
+
+        return super.onCreateDialog(id);
+    };
+
+    DialogInterface.OnMultiChoiceClickListener myItemsMultiClickListener = new DialogInterface.OnMultiChoiceClickListener() {
+        @Override
+        public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+            ListView lv = ((AlertDialog) dialog).getListView();
+            //Log.d( "tag" , "which = " + which + ", isChecked = " + isChecked);
+        }
+    };
+
+    DialogInterface.OnClickListener myBtnClickListener = new DialogInterface.OnClickListener() {
+        public void onClick(DialogInterface dialog, int which) {
+            SparseBooleanArray sbArray = ((AlertDialog)dialog).getListView().getCheckedItemPositions();
+            for (int i = 0; i < sbArray.size(); i++) {
+                int key = sbArray.keyAt(i);
+                if (sbArray.get(key))
+                    daysSelected = daysSelected + days[key].substring(0,3) + ", ";
+                    // Log.d("qwe", "checked: " + key);
+            }
+            daysSelected = daysSelected.substring(0, daysSelected.length() - 2);
+            trainingDays.setText(daysSelected);
+        }
+    };
+
+    TimePickerDialog.OnTimeSetListener myCallBack = new TimePickerDialog.OnTimeSetListener() {
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            myHour = hourOfDay;
+            myMinute = minute;
+            String normalizedMinute = "";
+            if(minute<10)
+            {normalizedMinute = "0" + Integer.toString(myMinute);}
+            else
+            {normalizedMinute = Integer.toString(myMinute);}
+            dayStartTime.setText(Integer.toString(myHour) + ":" + normalizedMinute);
+        }
+    };
+
+    TimePickerDialog.OnTimeSetListener mySecondCallBack = new TimePickerDialog.OnTimeSetListener() {
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            myHour = hourOfDay;
+            myMinute = minute;
+            String normalizedMinute = "";
+            if(minute<9)
+            {normalizedMinute = "0" + Integer.toString(myMinute);}
+            else
+            {normalizedMinute = Integer.toString(myMinute);}
+            trainingStart.setText(Integer.toString(myHour) + ":" + normalizedMinute);
+        }
+    };
+
+
+    public void hideKeyboard(View view) {
+        InputMethodManager inputMethodManager =(InputMethodManager)getSystemService(Settings.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
 }
