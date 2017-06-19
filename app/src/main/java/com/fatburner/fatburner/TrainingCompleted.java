dd6a12f2c3440822a75d0653063ae114c8f232d1
sete <steperchuk@gmail.com>
2017-06-19 18:05:26 +0300

    Training Completed activity UI updated.

--- app/src/main/java/com/fatburner/fatburner/TrainingCompleted.java
+++ app/src/main/java/com/fatburner/fatburner/TrainingCompleted.java
@@ -1,17 +1,27 @@
 package com.fatburner.fatburner;
 
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.Button;
+
+import com.github.lzyzsd.circleprogress.DonutProgress;
+
+import static android.R.color.holo_orange_dark;
 
 /**
  * Created by sergeyteperchuk on 6/13/17.
  */
 
 public class TrainingCompleted extends Menu {
+
+    DonutProgress doneBtn;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,14 +30,22 @@ public class TrainingCompleted extends Menu {
         View contentView = inflater.inflate(R.layout.activity_training_completed, null, false);
         mDrawerLayout.addView(contentView, 0);
 
-        Button okBtn = (Button) findViewById(R.id.ok_btn);
+        doneBtn = (DonutProgress) findViewById(R.id.done_btn);
+        doneBtn.setText("Got It");
+        startAnimation ();
 
-
-        okBtn.setOnClickListener(new View.OnClickListener() {
+        doneBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                     Intent intent = new Intent(TrainingCompleted.this, MainActivity.class);
                     startActivity(intent);
             }
         });
+    }
+
+    void startAnimation () {
+        AnimatorSet set = (AnimatorSet) AnimatorInflater.loadAnimator(TrainingCompleted.this, R.animator.progress_anim);
+        set.setInterpolator(new DecelerateInterpolator());
+        set.setTarget(doneBtn);
+        set.start();
     }
 }
