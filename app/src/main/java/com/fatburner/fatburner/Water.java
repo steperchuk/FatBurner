54d4899b91aa51bde6cf8e805f6e9ea3df07b877
sete <steperchuk@gmail.com>
2017-07-07 12:22:46 +0300

    Water layout changed. Need to fix and update class

--- app/src/main/java/com/fatburner/fatburner/Water.java
+++ app/src/main/java/com/fatburner/fatburner/Water.java
@@ -3,13 +3,21 @@ package com.fatburner.fatburner;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import com.github.lzyzsd.circleprogress.CircleProgress;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Created by sete on 6/15/2017.
@@ -30,6 +38,7 @@ public class Water extends Menu implements View.OnClickListener {
     float increment = 0;
     int amount = 0;
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,13 +58,64 @@ public class Water extends Menu implements View.OnClickListener {
         biggest = (ImageButton) findViewById(R.id.biggestWaterBtn);
         biggest.setOnClickListener(this);
 
+        ImageView imageView = (ImageView) findViewById(R.id.imageView) ;
+        String filename = "drop.png";
+        InputStream inputStream = null;
+        try{
+            inputStream = getApplicationContext().getAssets().open(filename);
+            Drawable d = Drawable.createFromStream(inputStream, null);
+            imageView.setImageDrawable(d);
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                if (inputStream != null)
+                    inputStream.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
 
+        //setting ImageView size
+        imageView.getLayoutParams().height = 50;
+        imageView.getLayoutParams().width = 50;
+        imageView.requestLayout();
 
         waterProgress = (CircleProgress) findViewById(R.id.waterProgress);
         waterProgress.setTextSize(100);
 
 
+        SeekBar seekBar = (SeekBar)findViewById(R.id.seekBar);
+        seekBar.setMax(10);
+        seekBar.setProgress(3,true);
 
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser) {
+                    if (progress >= 0 && progress <= seekBar.getMax()) {
+
+                        String progressString = String.valueOf(progress * 50);
+                        //seekBar.setText(progressString); // the TextView Reference
+                        seekBar.setSecondaryProgress(progress);
+                    }
+                }
+
+            }
+        });
 
 
         waterProgress.setOnClickListener(new View.OnClickListener() {
